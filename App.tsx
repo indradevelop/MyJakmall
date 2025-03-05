3590c993e91ca65d26d9706e3c3ac9744fa5a1b3
indralesmana <indradevelop@gmail.com>
2025-03-05 10:38:46 +0700

    init commit

--- App.tsx
+++ App.tsx
@@ -1,131 +1,240 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* eslint-disable react-native/no-inline-styles */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { useEffect, useState, useRef } from 'react';
 import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  SafeAreaView,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import { FlatList, TouchableOpacity, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
+import { Alert } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [categories, setCategories] = useState<string[]>([]);
+  const [showJokes, setShowJokes] = useState<string[]>([]);
+  const [jokesItem, setJokesItem] = useState<{ [key: string]: [] }>({});
+  const [clickCount, setClickCount] = useState<{ [key: string]: number }>({});
+  const flatListRef = useRef<FlatList>(null);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const fetchCategories = async () => {
+    try {
+      const url = 'https://v2.jokeapi.dev/categories';
+      console.log(`url ${url}`);
+      const response = await fetch(url);
+      const data = await response.json();
+      return data.categories;
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+      return [];
+    }
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  useEffect(() => {
+    const getCategories = async () => {
+      const data = await fetchCategories();
+      setCategories(data);
+    };
+    getCategories();
+  }, []);
+
+  const fetchJokes = async (category: string) => {
+    try {
+      const url = `https://v2.jokeapi.dev/joke/${category}?type=single&amount=2`;
+      console.log(`url ${url}`);
+      const response = await fetch(url);
+      const data = await response.json();
+      return data.jokes;
+    } catch (error) {
+      console.error('Error fetching jokes:', error);
+      return [];
+    }
+  };
+
+  const toggleJokes = async (item: string) => {
+    // console.log('jokesItem ' + JSON.stringify(jokesItem));
+    if (jokesItem[item] == null) {
+      const data = await fetchJokes(item);
+      setJokesItem((prevJokes: any) => {
+        var newJokes = { ...prevJokes };
+        newJokes[item] = data;
+        return newJokes;
+      });
+    }
+    if (showJokes.includes(item)) {
+      setShowJokes(showJokes.filter((i) => i !== item));
+    } else {
+      setShowJokes([...showJokes, item]);
+    }
+  };
+
+  const moveItemToTop = (index: number) => {
+    setCategories((prevCategories) => {
+      const updatedCategories = [...prevCategories];
+      const [selectedItem] = updatedCategories.splice(index, 1);
+      updatedCategories.unshift(selectedItem);
+      return updatedCategories;
+    });
+    flatListRef.current?.scrollToOffset({ animated: true, offset: 0 });
+
+  };
+
+  const addMoreData = async (item: string) => {
+    const moreJokes = await fetchJokes(item);
+    setJokesItem((prevJokes: any) => {
+      var newJokes = { ...prevJokes };
+      newJokes[item] = [...newJokes[item], ...moreJokes];
+      return newJokes;
+    });
+
+    setClickCount((prevCount) => {
+      const newCount = { ...prevCount };
+      newCount[item] = (newCount[item] ?? 0) + 1;
+      return newCount;
+    });
+
+  };
+
+  const showDialog = (item: string) => {
+    Alert.alert(
+      '',
+      `${item}`,
+      [{ text: 'OK' }],
+      { cancelable: true }
+    );
+  };
+
+
+  const renderItem = ({ item, index }: { item: string, index: number }) => {
+    return (
+      <View>
+        <TouchableWithoutFeedback onPress={() => toggleJokes(item)}>
+          <View style={styles.itemContainer}>
+            <Text style={{
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}>{index + 1}.</Text>
+            <Text style={styles.itemText}>{item}</Text>
+            {index !== 0 ? (
+              <TouchableOpacity style={styles.button} onPress={() => moveItemToTop(index)}>
+                <Text style={styles.buttonText}>Go Top</Text>
+              </TouchableOpacity>) : (<View style={styles.buttonTop}><Text style={styles.buttonText}>Top</Text></View>)}
+
+            <View style={styles.buttonIcon}>
+              <FontAwesome6
+                name={showJokes.includes(item) ? 'angle-up' : 'angle-down'}
+                size={24}
+                iconStyle={'solid'}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+        {showJokes.includes(item) && (
+          <View style={styles.jokesContainer}>
+            {jokesItem[item]?.map((joke: any, idx: number) => (
+              <TouchableOpacity key={idx} onPress={() => showDialog(joke.joke)}>
+                <Text style={styles.jokeText}>{joke.joke}</Text>
+              </TouchableOpacity>
+            ))}
+            {(clickCount[item] === undefined || clickCount[item] < 2) && jokesItem[item]?.length > 0 && (<TouchableOpacity style={styles.buttonAddMore} onPress={() => addMoreData(item)}>
+              <Text style={styles.buttonText}>Add More Data</Text>
+            </TouchableOpacity>)}
+
+          </View>
+        )}
+      </View>
+    );
+  };
+  const styles = StyleSheet.create({
+    itemContainer: {
+      flexDirection: 'row',
+      alignContent: 'flex-start',
+      alignItems: 'center',
+      paddingHorizontal: 16,
+      paddingVertical: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ccc',
+    },
+    itemText: {
+      marginLeft: 16,
+      flex: 1,
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    buttonsContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 10,
+    },
+    buttonTop: {
+      marginLeft: 16,
+      backgroundColor: '#AAAAAA',
+      padding: 10,
+      borderRadius: 5,
+    },
+    buttonIcon: {
+      marginLeft: 16,
+      padding: 10,
+    },
+    buttonAddMore: {
+      backgroundColor: '#007BFF',
+      padding: 10,
+      alignItems: 'center',
+    },
+    button: {
+      marginLeft: 16,
+      backgroundColor: '#007BFF',
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: 'bold',
+    },
+    jokesContainer: {
+      marginTop: 10,
+      marginLeft: 32,
+    },
+    jokeText: {
+      fontSize: 16,
+      color: '#555',
+      borderBottomWidth: 1,
+      borderBottomColor: '#ccc',
+      paddingVertical: 10,
+      paddingRight: 16,
+    },
+  });
+
+  const onRefresh = async () => {
+    const data = await fetchCategories();
+    setCategories(data);
+    setShowJokes([]);
+    setJokesItem({});
+    setClickCount({});
+    setIsRefreshing(false);
+  };
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <Text style={{ textAlign: 'center', fontSize: 24, fontWeight: 'bold', margin: 20 }}>
+          My Jakmall
+        </Text>
+        <FlatList
+          ref={flatListRef}
+          data={categories}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          onRefresh={onRefresh}
+          refreshing={isRefreshing}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
